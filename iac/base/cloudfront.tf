4b6f70e4a0d57fac29a0a46d3b2ec38208547a24
ta3wook <taewook02@mju.ac.kr>
2025-12-02 13:48:23 +0900

    SPA 라우팅을 위한 에러 페이지 리다이렉션 설정

--- iac/base/cloudfront.tf
+++ iac/base/cloudfront.tf
@@ -51,6 +51,18 @@ resource "aws_cloudfront_distribution" "qwik_frontend_cdn" {
     }
   }
 
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
   viewer_certificate {
     cloudfront_default_certificate = true # 기본 *.cloudfront.net 인증서 사용
   }
